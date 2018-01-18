68853d5a1372477aa33b66a478645b6811775360
Vladimir Feskov <v@vladimirfeskov.com>
2018-01-19 00:56:49 +0100

    fix(server): wrong duplicate repo response

--- server/src/api-middleware/repos.js
+++ server/src/api-middleware/repos.js
@@ -6,7 +6,7 @@ export async function create ({ body, token }, res, next) {
   if (!token) { return next(unauthorized()) }
   if (!body || !validRepo(body.repo)) { return next(badRequest()) }
   const { repos } = await loadDb(token)
-  if (repos.includes(body.repo)) { return success(res, repos) }
+  if (repos.includes(body.repo)) { return success(res, { repos }) }
   const attrs = { repos: [body.repo].concat(repos) }
   await updateDb(token, attrs)
   success(res, attrs)
@@ -16,7 +16,7 @@ export async function remove ({ params, token }, res, next) {
   if (!token) { return next(unauthorized()) }
   if (!params || !validRepo(params.repo)) { return next(badRequest()) }
   const { repos } = await loadDb(token)
-  if (!repos.includes(params.repo)) { return success(res, repos) }
+  if (!repos.includes(params.repo)) { return success(res, { repos }) }
   const attrs = { repos: repos.filter(r => r !== params.repo) }
   await updateDb(token, attrs)
   success(res, attrs)
