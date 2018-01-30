27bc75475092c6a89d69b3b0737bb35d3945d6bd
Vladimir Feskov <v@vladimirfeskov.com>
2018-01-30 18:29:15 +0100

    feat(notifier): enable daily checks

--- notifier/src/parts/loadUsers.ts
+++ notifier/src/parts/loadUsers.ts
@@ -2,7 +2,16 @@ import { DBUser } from './interfaces'
 import { Collection } from 'mongodb'
 
 export default async function loadUsers (collection: Collection) {
-  const dbUsers = await collection.find({ watching: true }, {
+  const utcHour = new Date().getUTCHours()
+  const query = {
+    watching: true,
+    $or: [
+      { frequency: { $exists: false } },
+      { frequency: 'hourly' },
+      { frequency: 'daily', checkAt: utcHour }
+    ]
+  }
+  const dbUsers = await collection.find(query, {
     projection: { passwordEncrypted: 0 }
   }).toArray()
   return dbUsers.map(user => {
