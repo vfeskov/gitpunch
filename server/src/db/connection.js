5038e22173975a73db9d3eb61a8df8f1d8d48a94
Vladimir Feskov <v@vladimirfeskov.com>
2018-09-08 14:31:50 +0200

    build(server): update dependencies

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -3,6 +3,6 @@ const URL = process.env.WAB_MONGODB_URL
 const DB_NAME = process.env.WAB_MONGODB_DBNAME
 
 export const connection = (async () => {
-  const client = await MongoClient.connect(URL)
+  const client = await MongoClient.connect(URL, { useNewUrlParser: true })
   return client.db(DB_NAME)
 })()
