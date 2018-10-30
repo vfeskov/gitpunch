b92e96787545ea87dc5da9d6068cde0f8b408c6f
Vladimir Feskov <v@vladimirfeskov.com>
2018-10-30 22:29:24 +0100

    fix(client): base oauth link on NODE_ENV

--- client/src/services/oauth.js
+++ client/src/services/oauth.js
@@ -1,7 +1,7 @@
 export function oauthUrl ({ repos, returnTo }) {
   // eslint-disable-next-line no-restricted-globals
   let { hostname, protocol, port } = location
-  if (hostname === 'localhost') { port = 3001 }
+  if (process.env.NODE_ENV === 'development' && port === 3000) { port = 3001 }
   let params = [];
   if (repos && repos.length) { params.push(`repos=${JSON.stringify([...repos].reverse())}`); }
   if (returnTo) { params.push(`returnTo=${returnTo}`)}
