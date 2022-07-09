e2bd4ce33fc97c94df88316841a70b643f1ca00c
Vlad Feskov <v@vladimirfeskov.com>
2022-07-09 11:53:26 +0200

    Update deploy-docker-image.sh

--- .circleci/deploy-docker-image.sh
+++ .circleci/deploy-docker-image.sh
@@ -4,18 +4,18 @@ APPLICATION_NAME=vfeskov.com
 APPLICATION_ENV=main2
 DOCKERRUN_FILE=${DOCKER_TAG}-Dockerrun.aws.json
 
-# 1. generate Dockerrun.aws.json
+echo "Generating ${DOCKERRUN_FILE}"
 eval "cat > ${DOCKERRUN_FILE} <<EOF
 $(<$(dirname $0)/Dockerrun.aws.json.template)
 EOF
 " 2> /dev/null
 
-# 2. upload it to S3
+echo "Uploading to s3://${DEPLOYMENT_BUCKET}/${DOCKERRUN_FILE}"
 aws s3 cp $DOCKERRUN_FILE "s3://${DEPLOYMENT_BUCKET}/${DOCKERRUN_FILE}"
 
-# 3. create new elasticbeanstalk application version
+echo "Creating Elastic Beanstalk Application Version $DOCKER_TAG of $APPLICATION_NAME"
 aws elasticbeanstalk create-application-version --region=$REGION --application-name $APPLICATION_NAME \
     --version-label $DOCKER_TAG --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$DOCKERRUN_FILE
 
-# 4. deploy the version
+echo "Deploying the version to $APPLICATION_ENV"
 aws elasticbeanstalk update-environment --region=$REGION --environment-name $APPLICATION_ENV --version-label $DOCKER_TAG
